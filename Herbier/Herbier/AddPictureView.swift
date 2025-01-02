445a2f8ac9347f72293238b0e23342834e9be3d2
florian-dreue <florian.dreue@etud.u-picardie.fr>
2025-01-02 16:56:23 +0100

    AddScalability
    Ajout de l'adaptation à la taille de l'écran

--- Herbier/Herbier/AddPictureView.swift
+++ Herbier/Herbier/AddPictureView.swift
@@ -19,45 +19,46 @@ struct AddPictureView: View {
     @State private var selectedDate: Date = Date()
 
     var body: some View {
-        VStack() {
-            if let selectedImage = selectedImage {
-                Image(uiImage: selectedImage)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 300, height: 300)
-                    .shadow(radius: 10)
-            } else {
-                Text("Aucune image sélectionnée")
-                    .foregroundColor(.gray)
-                    .multilineTextAlignment(.center)
-                    .frame(width: 300, height: 300)
+        GeometryReader { geometry in
+            VStack() {
+                if let selectedImage = selectedImage {
+                    Image(uiImage: selectedImage)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geometry.size.width * 0.75, height: geometry.size.height * 0.4)
+                        .shadow(radius: 10)
+                } else {
+                    Text("Aucune image sélectionnée")
+                        .foregroundColor(.gray)
+                        .multilineTextAlignment(.center)
+                        .frame(width: geometry.size.width * 0.75, height: geometry.size.height * 0.4)
+                }
+                Spacer()
+                    .frame(height: geometry.size.height * 0.01)
+                Button("Choisir une photo") {
+                    isImagePickerPresented.toggle()
+                }
+                DatePicker(selection: $selectedDate,
+                           in: ...currentDate,
+                           displayedComponents: [.date],
+                           label: { /*@START_MENU_TOKEN@*/Text("Date")/*@END_MENU_TOKEN@*/ })
+                .frame(width: 300)
+                TextField("Nom de la photo", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .multilineTextAlignment(TextAlignment.center)
+                Picker(selection: $selectedCategory, label: Text("Catégorie")) {
+                    Text("Sélectionner une catégorie").tag("")
+                    Text("Animal").tag("animal")
+                    Text("Végétal").tag("vegetal")
+                    Text("Fongieux").tag("fungieux")
+                }
+                Button("Ajouter") {
+                    addItem()
+                }
             }
-            Spacer()
-                .frame(height: 20)
-            Button("Choisir une photo") {
-                isImagePickerPresented.toggle()
-            }
-            DatePicker(selection: $selectedDate,
-                       in: ...currentDate,
-                       displayedComponents: [.date],
-                       label: { /*@START_MENU_TOKEN@*/Text("Date")/*@END_MENU_TOKEN@*/ })
-            .frame(width: 300)
-            TextField("Nom de la photo", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-                .multilineTextAlignment(TextAlignment.center)
-            Picker(selection: $selectedCategory, label: Text("Catégorie")) {
-                Text("Sélectionner une catégorie").tag("")
-                Text("Animal").tag("animal")
-                Text("Végétal").tag("vegetal")
-                Text("Fongieux").tag("fungieux")
-            }
-            Button("Ajouter") {
-                addItem()
+            .sheet(isPresented: $isImagePickerPresented) {
+                ImagePicker(selectedImage: $selectedImage, isImagePickerPresented: $isImagePickerPresented)
             }
         }
-        .sheet(isPresented: $isImagePickerPresented) {
-            ImagePicker(selectedImage: $selectedImage, isImagePickerPresented: $isImagePickerPresented)
-        }
-        .padding(.horizontal, 30.0)
     }
     
     private func addItem() {
--- Herbier/Herbier/HistoryView.swift
+++ Herbier/Herbier/HistoryView.swift
@@ -6,61 +6,86 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct HistoryView: View {
     
+    @Environment(\.modelContext) private var modelContext
     @State private var showDetails : Bool = false;
+    @State private var temporaryTables: [TemporaryTable] = []
     
     var body: some View {
-        if(showDetails) {
-            /*DetailView()*/
-            VStack() {
-                Image(systemName: "xmark")
-                    .onTapGesture {
-                        showDetails = false
-                    }
-                Image("PhotoTest")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 300, height: 300)
-                    .shadow(radius: 10)
-                Spacer()
-                    .frame(height: 20)
-                Text("Date de la photo")
-                .frame(width: 300)
-                Text("Nom de la photo")
-                Text("Type de la photo")
-            }
-        }
-        else {
-            VStack {
-                ScrollView {
-                    ForEach (0..<10) { _ in
-                        HStack {
-                            Image("PhotoTest")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: (100), height: 100)
-                            VStack {
-                                Text("Nom de la photo")
-                                Spacer()
-                                Text("Date")
-                            }
-                            .padding(.vertical, 20.0)
-                            Text("Type")
-                        }
-                        .padding(.horizontal, 20.0)
-                        .border(Color.black, width: 1)
+        GeometryReader { geometry in
+            if(showDetails) {
+                /*DetailView()*/
+                VStack() {
+                    Image(systemName: "xmark")
                         .onTapGesture {
-                            print("HStack cliqué!")
-                            showDetails = true
+                            showDetails = false
+                        }
+                    Image("PhotoTest")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geometry.size.width * 0.75, height: geometry.size.height * 0.4)
+                        .shadow(radius: 10)
+                    Spacer()
+                        .frame(height: 20)
+                    Text("Date de la photo")
+                        .frame(width: geometry.size.width * 0.75)
+                    Text("Nom de la photo")
+                    Text("Type de la photo")
+                }
+            }
+            else {
+                VStack(alignment: .center) {
+                    ScrollView {
+                        ForEach (0..<10) { table in
+                            HStack {
+                                Image("PhotoTest")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: geometry.size.width * 0.40, height: geometry.size.height * 0.20)
+                                Spacer()
+                                VStack {
+                                    Text("Nom de la photo")
+                                    Spacer()
+                                    Text("Date")
+                                }
+                                .padding(.vertical, 20.0)
+                                Spacer()
+                                Text("Type")
+                            }
+                            .padding(.horizontal, 20.0)
+                            .border(Color.black, width: 1)
+                            .onTapGesture {
+                                print("HStack cliqué!")
+                                showDetails = true
+                            }
                         }
                     }
+                }
+                .onAppear {
+                    fetchTemporaryTables()
                 }
             }
         }
     }
-    private func test() {
+    
+    // Fonction pour récupérer les données de TemporaryTable
+    private func fetchTemporaryTables() {
+        Task {
+            do {
+                print("Recup Data")
+                let fetchDescriptor = FetchDescriptor<TemporaryTable>()
+                
+                let tables: [TemporaryTable] = try modelContext.fetch(fetchDescriptor)
+                
+                temporaryTables = tables
+                print("Final Data ", temporaryTables)
+            } catch {
+                print("Erreur lors de la récupération des données: \(error)")
+            }
+        }
     }
 }
 
